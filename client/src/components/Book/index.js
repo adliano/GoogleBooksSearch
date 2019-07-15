dae56bb544cef32fd5caea5c9eb65710e9b5c570
Adliano Alves <adliano@me.com>
2019-07-14 21:55:19 -0700

    working on book card

--- client/src/components/Book/index.js
+++ client/src/components/Book/index.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import { H2, H5, P } from '../headers'
 
-import Box from '@material-ui/core/Box'
-import IconButton from '@material-ui/core/IconButton'
+import Box, { 
+    IconButton,
+    InputAdornment,
+    Grid,
+    CardMedia,
+    Card,
+    CardContent,
+    Typography,
+    Paper
+} from '@material-ui/core/'
+
 import Visibility from '@material-ui/icons/Visibility'
 import Save from '@material-ui/icons/Save'
-import InputAdornment from '@material-ui/core/InputAdornment'
-import Grid from '@material-ui/core/Grid'
-import CardMedia from '@material-ui/core/CardMedia'
-import Card from '@material-ui/core/Card'
 
 import { makeStyles } from '@material-ui/core/styles'
-
-import Paper from '@material-ui/core/Paper'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -37,78 +40,26 @@ function Book () {
    * Return
    */
   return (
-    <div>
-      <Box borderTop={1} borderColor='grey.500'>
-        {/* <Grid container direction='row' justify='center' alignItems='center'> */}
-        {/* *********** Top Row *********** */}
-        <Grid container direction='row' justify='center' alignItems='center'>
-          {/* Grid with the Book info */}
-          <Grid
-            item
-            xs={3}
-            style={{
-              // backgroundColor: 'green',
-              // color: 'white',
-              padding: '25px'
-            }}
-          >
-            <H5>Book Name</H5>
-            <P>the book it great to read it</P>
-            <P>Someone wrote it</P>
-          </Grid>
-          {/*  Empty grid to use as spacer */}
-          <Grid item xs={6} />
-          {/* Grid with the buttons */}
-          <Grid
-            item
-            xs={3}
-            style={{
-              //   backgroundColor: 'blue',
-              //   color: 'white',
-              textAlign: 'center',
-              padding: '25px'
-            }}
-          >
-            <IconButton style={{ marginRight: '25px' }}>
-              <Visibility />
-            </IconButton>
-            <IconButton style={{ marginRight: '25px' }}>
-              <Save />
-            </IconButton>
-          </Grid>
-        </Grid>
-        {/* *********** Top Row *********** */}
-        <Grid container direction='row' justify='center' alignItems='center'>
-          <Grid
-            item
-            xs={4}
-            style={{
-              backgroundColor: 'blue',
-              textAlign: 'center',
-              padding: '25px'
-            }}
-          >
-            <Card>
-              <CardMedia
-                image='https://via.placeholder.com/350x350'
-                title='Contemplative Reptile'
-              />
-            </Card>
-          </Grid>
-          <Grid
-            item
-            xs={8}
-            style={{
-              backgroundColor: 'yellow',
-              textAlign: 'center',
-              padding: '25px'
-            }}
-          >
-            X4
-          </Grid>
-        </Grid>
-      </Box>
-    </div>
+    <>
+      <Card className={classes.card}>
+        <div style={{display: 'flex', flexDirection: 'column', }}>
+          <CardContent className={classes.content}>
+            <Typography component='h5' variant='h5'>
+              Book Name
+            </Typography>
+            <Typography variant='subtitle1' color='textSecondary'>
+              Book Greatting
+            </Typography>
+            <Typography variant='subtitle1' color='textSecondary'>
+              Author
+            </Typography>
+          </CardContent>
+        </div>
+        <CardMedia  style={{ height: 150,  width: 150, paddingTop: '15%'}}
+        image="https://via.placeholder.com/150x150"
+        />
+      </Card>
+    </>
   )
 }
 
