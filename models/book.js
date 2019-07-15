6dbe997eab3d282b144ee2b205e2f4d9ff1d59fa
Adliano Alves <adliano@me.com>
2019-07-15 02:42:28 -0700

    changed schemas

--- models/book.js
+++ models/book.js
@@ -3,9 +3,10 @@ const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
   title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
+  author: { type: Array, required: true },
+  description: String,
+  previewLink: String,
+  thumbnail: String,
 });
 
 const Book = mongoose.model("Book", bookSchema);
