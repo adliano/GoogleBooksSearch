c28582e6c888269c86d7f3bffb03e1b9f87d285a
Adliano Alves <adliano@me.com>
2019-07-15 11:28:30 -0700

    started the fetch for google books api

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,66 @@
 import React, { Component }from 'react'
-
+// 
 import Nav from './components/Nav'
 import Paper from './components/paper'
-import SearchFragment from './pages/SearchFragment'
 import { H2, H5, P } from './components/headers'
 import BookShelve from './components/BookShelve'
-
+import Book from './components/Book'
+import SearchFragment from './pages/SearchFragment'
+// 
 import {
   TextField,
   Box,
   IconButton,
   InputAdornment,
 } from '@material-ui/core/'
-
+// 
 import {
   Search,
- } from '@material-ui/icons/'
-
+} from '@material-ui/icons/'
+// 
+// ************* DotEnv ******************
+import dotenv from 'dotenv'
+import { GoogleBooksAPI as Keys } from './utils/Key'
+// 
+dotenv.config()
+// ***************************************
 
 class App extends Component {
 
+  state = {
+    booksResults:[],
+  }
+
   componentDidMount(){
     // TODO: Fetch data from google api here
-    let _URL = `https://www.googleapis.com/books/v1/volumes?q=` + 'bookname'
+    this.searchGoogleBooks('harry potter')
+  }
+  /**
+   * searchGoogleBooks
+   * Method to get books from google API
+   */
+  searchGoogleBooks = (query) => {
+    fetch(`${Keys.URL}?q=${query}`)
+    .then(response => response.json())
+    .then(results => results.items)
+    .then(booksArray => {
+      //
+      // console.log(booksArray[0].volumeInfo)
+      //
+      let _booksElements = booksArray.map(({ volumeInfo: { title, author, previewLink, thumbnail, description } }) => {
+        
+        console.log(title)
+        console.log(author)
+        console.log(previewLink)
+        console.log(thumbnail)
+        console.log(description)
+        
+      })
+      // <Book title='' author='' previewLink='' thumbnail='' description=""/>
+
+      // //
+      // this.setState({ booksResults: booksArray })
+    })
   }
   /**
    * Render
---
+++ client/src/utils/Key.js
@@ -0,0 +1,6 @@
+
+
+exports.GoogleBooksAPI = {
+    URL: 'https://www.googleapis.com/books/v1/volumes',
+    key: process.env.API_KEY,
+}
